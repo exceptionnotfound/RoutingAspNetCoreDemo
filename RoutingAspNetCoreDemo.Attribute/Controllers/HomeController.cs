b00c1d53656bb2b488e38640111f38f4dcb80472
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2019-12-13 09:59:18 -0700

    Added skeleton Attribute project and new Area example for Conventional MVC project.

---
+++ RoutingAspNetCoreDemo.Attribute/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RoutingAspNetCoreDemo.Attribute.Models;
+
+namespace RoutingAspNetCoreDemo.Attribute.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("~/")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Home/Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- RoutingAspNetCoreDemo.Attribute/Startup.cs
+++ RoutingAspNetCoreDemo.Attribute/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace RoutingAspNetCoreDemo.Conventional
+namespace RoutingAspNetCoreDemo.Attribute
 {
     public class Startup
     {
@@ -23,9 +23,6 @@ namespace RoutingAspNetCoreDemo.Conventional
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //The below method is new in ASP.NET Core 3.0.  
-            //It adds to the service layer the services for controllers and views.
-            //However, it does not add any services for Razor Pages or SignalR.
             services.AddControllersWithViews();
         }
 
@@ -49,26 +46,9 @@ namespace RoutingAspNetCoreDemo.Conventional
 
             app.UseAuthorization();
 
-            //In ASP.NET Core 3.0, we use UseEndpoints instead of UseMvc, UseSignalR, UseRazorPages, etc.
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapAreaControllerRoute(name: "blog",
-                                                 areaName: "blog",
-                                                 pattern: "blog/{controller=Post}/{action=Index}/{id?}");
-
-                //The below method establishes a "convention" route for an MVC app.
-                //It will match anything with the format controller/action/id, where id is optional
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                //This method establishes a different "convention" route, one that only goes to the Account/Index action.
-                endpoints.MapControllerRoute("account", "account/{id?}",
-                                                defaults: new { controller = "Account", action = "Index" });
-
-                //This method established yet another "convention" route, for the User/Index action.
-                endpoints.MapControllerRoute("user", "user/{id?}",
-                                             defaults: new { controller = "User", action = "Index" });
+                endpoints.MapControllers()
             });
         }
     }
